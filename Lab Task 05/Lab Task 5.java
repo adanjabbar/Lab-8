b26324eba46c23abe69d11164f8021b7e72978f8
adanjabbar <adanjabbar1352@gmail.com>
2025-11-17 10:13:58 +0500

    Update Lab Task 5.java

--- Lab Task 05/Lab Task 5.java
+++ Lab Task 05/Lab Task 5.java
@@ -1,7 +1,7 @@
-Write a method that counts the number of letters in a string using the following header: 
+/*Write a method that counts the number of letters in a string using the following header: 
 public static int countLetters(String s) 
 Write a test program that prompts the user to enter a string and displays the number of letters in the 
-string. 
+string. */
 
   import java.util.Scanner;
 
