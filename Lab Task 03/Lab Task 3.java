83175610cfcc4f2326a76a80827a1a0f12f87591
adanjabbar <adanjabbar1352@gmail.com>
2025-11-17 10:12:01 +0500

    Update Lab Task 3.java

--- Lab Task 03/Lab Task 3.java
+++ Lab Task 03/Lab Task 3.java
@@ -1,8 +1,8 @@
-Write a method with the following header to display three numbers in increasing order: 
+/*Write a method with the following header to display three numbers in increasing order: 
 public static void displaySortedNumbers(double num1, double num2, 
 double num3) 
 Write a test program that prompts the user to enter three numbers and invokes the method to display them 
-in increasing order. 
+in increasing order. */
 
   import java.util.Scanner;
 
