a0768b85603509b0d0fc3deae90c00365341330f
adanjabbar <adanjabbar1352@gmail.com>
2025-11-17 10:11:28 +0500

    Update Lab Task 4.java

--- Lab Task 04/Lab Task 4.java
+++ Lab Task 04/Lab Task 4.java
@@ -1,6 +1,6 @@
-Write a method that returns the number of days in a year using the following header: 
+/*Write a method that returns the number of days in a year using the following header: 
 public static int numberOfDaysInAYear(int year) 
-Write a test program that displays the number of days in year from 2000 to 2020.
+Write a test program that displays the number of days in year from 2000 to 2020.*/
 
   public class LabTask4 {
     public static void main(String[] args){
