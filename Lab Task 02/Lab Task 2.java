4de778ae112a7108f243555293e2ffef4243d2a8
adanjabbar <adanjabbar1352@gmail.com>
2025-11-17 10:10:49 +0500

    Update Lab Task 2.java

--- Lab Task 02/Lab Task 2.java
+++ Lab Task 02/Lab Task 2.java
@@ -1,11 +1,11 @@
-Write the methods with the following headers 
+/*Write the methods with the following headers 
 // Return the reversal of an integer, i.e., reverse(456) returns 654 
 public static int reverse(int number) 
 // Return true if number is a palindrome 
 public static boolean isPalindrome(int number) 
 Use the reverse method to implement isPalindrome. A number is a palindrome if its reversal is the same 
 as itself. Write a test program that prompts the user to enter an integer and reports whether the integer is 
-a palindrome.
+a palindrome.*/
 
   import java.util.Scanner;
 
